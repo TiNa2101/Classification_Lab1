6a0d50a3204676a69284e4558bea634392faaf6a
TiNa2101 <71177685+TiNa2101@users.noreply.github.com>
2022-10-16 23:52:48 +0300

    Update train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -3,12 +3,12 @@ from catboost import CatBoostClassifier
 from sklearn.multioutput import MultiOutputClassifier
 
 
-def train(X_train: pd.DataFrame, y_train: pd.DataFrame) -> MultiOutputClassifier:
+def train(train_data: pd.DataFrame, train_target: pd.DataFrame) -> MultiOutputClassifier:
     cat_model = CatBoostClassifier(n_estimators=100,
                                    depth=8,
                                    learning_rate=1, 
                                    loss_function='Logloss')
     model = MultiOutputClassifier(cat_model, n_jobs=1)
-    model.fit(X_train, y_train)
+    model.fit(train_data, train_target)
     
     return model
